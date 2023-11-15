f99130b53bc08643e024509c3a47958187ac31b6
GorgeThePotato <perez.jorge.rodriguez@gmail.com>
2023-11-15 10:57:13 +0100

    5.23

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -28,6 +28,18 @@ describe('Blog', function() {
 
     cy.contains('SuperAdminUser logged in')
   })
+ 
+  it('login fails with wrong password', function() {
+    cy.contains('login').click()
+    cy.get('#username').type('wrong')
+    cy.get('#password').type('wrong')
+    cy.get('#login-button').click()
+
+    cy.get('.error')
+      .should('contain', 'Wrong username or password')
+      .and('have.css', 'color', 'rgb(255, 0, 0)')
+      .and('have.css', 'border-style', 'solid')
+  })
 
   describe('when logged in', function() {
     beforeEach(function() {
@@ -80,38 +92,50 @@ describe('Blog', function() {
     })
 
     it('blogs are ordered based on likes', function(){
+      cy.createBlog({title:'blog with 0 likes', author:'cypress',url:'cypress.com'})
       cy.createBlog({title:'blog with 4 likes', author:'cypress',url:'cypress.com'})
-      cy.get('.blog')
-        .contains('view')
-        .click()
-      cy.get('.blog_details')
-        .contains('like')
-        .click()
-        .click()
-        .click()
-        .click()
-
       cy.createBlog({title:'blog with 2 likes', author:'cypress',url:'cypress.com'})
+      
       cy.get('.blog')
         .eq(1)
         .contains('view')
         .click()
       cy.get('.blog_details')
+        .eq(1)
         .contains('like')
         .click()
+      cy.wait(2000)
+      cy.get('.blog_details')
+        .contains('like')
         .click()
+      cy.wait(2000)
+      cy.get('.blog_details')
+        .contains('like')
+        .click()
+      cy.wait(2000)
+      cy.get('.blog_details')
+        .contains('like')
+        .click()
+      cy.wait(2000)
+
+      cy.get('.blog')
+        .eq(2)
+        .contains('view')
+        .click()
+      cy.get('.blog_details')
+        .eq(2)
+        .contains('like')
+        .click()
+      cy.wait(2000)
+      cy.get('.blog_details')
+        .eq(1)
+        .contains('like')
+        .click()
+      cy.wait(2000)
+
+      cy.get('.blog').eq(0).should('contain', 'blog with 4 likes')
+      cy.get('.blog').eq(1).should('contain', 'blog with 2 likes')
+      cy.get('.blog').eq(2).should('contain', 'blog with 0 likes')
     })
-  })
-
-  it('login fails with wrong password', function() {
-    cy.contains('login').click()
-    cy.get('#username').type('wrong')
-    cy.get('#password').type('wrong')
-    cy.get('#login-button').click()
-
-    cy.get('.error')
-      .should('contain', 'Wrong username or password')
-      .and('have.css', 'color', 'rgb(255, 0, 0)')
-      .and('have.css', 'border-style', 'solid')
   })
 })
